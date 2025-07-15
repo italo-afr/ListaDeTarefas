539a567affa3f6272fe001f1c1eae9cbf1d867e8
italo-afr <italoafr1@gmail.com>
2025-07-15 09:59:19 -0300

    Adicionando o creteTarefa ao Nova Tarefa

--- src/components/GetSupabase/AllService.tsx
+++ src/components/GetSupabase/AllService.tsx
@@ -3,12 +3,13 @@ import { supabase } from "../../config/supabaseClient";
 type NewTask = {
     title: string;
     description: string;
+    date_check: string;
 };
 
 export const createTask = async (taskData: NewTask) => {
     const { data, error } = await supabase
         .from('tableList')
-        .insert([{ title: taskData.title, description: taskData.description, completed: false, arquivado: false }])
+        .insert([{ title: taskData.title, description: taskData.description, date_check: taskData.date_check }])
         .select()
         .single();
     return { data, error };
---
+++ src/pages/Dashboard/NovaTarefa/NovaTarefaPage.tsx
@@ -0,0 +1,90 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './NovaTarefaPage.module.css';
+import { createTask } from '../../../components/GetSupabase/AllService';
+import { useEffect, useRef } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { CalendarDays } from 'lucide-react';
+
+export function NovaTarefaPage() {
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [date_check, setDateCheck] = useState<Date | undefined>(undefined);
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+    const datePickerRef = useRef<HTMLDivElement>(null);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault(); // Impede o recarregamento da página
+
+        console.log('Formulário enviado. Título:', title, 'Descrição:', description);
+
+        const response = await createTask({
+            title: title,
+            description: description,
+            date_check: date_check ? date_check.toISOString() : ''
+        });
+
+        console.log('Resposta recebida do serviço:', response);
+
+        setTitle('');
+        setDescription('');
+        setDateCheck(undefined);
+
+        navigate('/dashboard/entrada');
+
+    };
+
+    // Efeito para fechar o calendário ao clicar fora dele
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            if (datePickerRef.current && !datePickerRef.current.contains(event.target as Node)) {
+                setIsDatePickerOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [datePickerRef]);
+
+    return (
+        <div className={styles.pageContainer}>
+            <div className={styles.titleForm}>
+                <h1>Criar Nova Tarefa</h1>
+            </div>
+            <p className={styles.description}>
+                Preencha os campos abaixo para criar uma nova tarefa.
+            </p>
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <label>Título</label>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+
+                <label>Descrição</label>
+                <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
+
+                <div className={styles.formGroup}>
+                    <label htmlFor="date-input">Data de Conclusão</label>
+                    <div className={styles.datePickerContainer}>
+                        <div className={styles.dateInputWrapper} onClick={() => setIsDatePickerOpen(true)}>
+                            <input id="date-input" type="text" readOnly value={date_check ? format(date_check, 'dd/MM/yyyy') : ''} placeholder="Selecione uma data" />
+                            <CalendarDays size={20} className={styles.calendarIcon} />
+                        </div>
+                        {isDatePickerOpen && (
+                            <div ref={datePickerRef} className={styles.datePickerPopover}>
+                                <DayPicker mode="single" selected={date_check} onSelect={(date) => {
+                                    setDateCheck(date); setIsDatePickerOpen(false); // Fecha ao selecionar
+                                }} locale={ptBR} fromYear={new Date().getFullYear()} />
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                <button type="submit" className={styles.submitButton}>Salvar tarefa</button>
+            </form>
+        </div>
+    );
+}
